b276ba569a7b00b431fe8bcc4d6ce16d15e2e21d
andyluak <49715292+andyluak@users.noreply.github.com>
2021-12-26 23:07:53 +0200

    Header

--- peregrine-shop/src/App.js
+++ peregrine-shop/src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shoppage/shop';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div >
+      <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
