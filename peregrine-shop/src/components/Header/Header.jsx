add558a39e36da35cbec095a32244f22302b8b23
andyluak <49715292+andyluak@users.noreply.github.com>
2021-12-27 16:34:16 +0200

    CartIcon / CartDropdown / CartReducer & actions

--- peregrine-shop/src/components/Header/Header.jsx
+++ peregrine-shop/src/components/Header/Header.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './header.scss';
-import { ReactComponent as Logo } from '../../assets/images/logo.svg'
+import { ReactComponent as Logo } from '../../assets/images/logo.svg';
+import CartIcon from '../CartIcon/CartIcon';
+import CartDropdown from '../CartDropdown/CartDropdown';
 import { auth } from '../../firebase/firebase.utils'
-const Header = ({ currentUser }) => {
-	console.log(currentUser);
+const Header = ({ currentUser, hidden }) => {
 	return (
 		<div className="header">
 			<Link className="logo-container" to="/">
@@ -28,13 +29,16 @@ const Header = ({ currentUser }) => {
 						SIGN IN
 					</Link>
 				)}
+				<CartIcon />
 			</div>
+			{hidden ? null : <CartDropdown />}
 		</div>
 	)
 }
 
-const mapStateToProps = state => ({
-	currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	currentUser,
+	hidden
 });
 
 export default connect(mapStateToProps)(Header);
