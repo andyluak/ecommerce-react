f304ea2bea6c69798fb785eefee96fa6e212f4d6
andyluak <49715292+andyluak@users.noreply.github.com>
2021-12-27 18:27:51 +0200

    Cart Item Component
    Connectet cart dropddown with reducer

--- peregrine-shop/src/components/CartDropdown/CartDropdown.jsx
+++ peregrine-shop/src/components/CartDropdown/CartDropdown.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './CartDropdown.scss';
 import CustomButton from '../CustomButton/CustomButton';
+import CartItem from '../CartItem/CartItem';
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
 	return (
 		<div className="cart-dropdown">
-			<div className="cart-items" />
+			<div className="cart-items" >
+				{cartItems.length ? (
+					cartItems.map(cartItem => {
+						return <CartItem key={cartItem.id} item={cartItem} />
+					})
+				) : (
+					<span className="empty-message">Your cart is empty</span>
+				)}
+			</div>
 			<CustomButton>GO TO CHECKOUT</CustomButton>
 		</div>
 	)
 }
 
-export default CartDropdown;
+const mapPropsToState = ({ cart: { cartItems } }) => ({
+	cartItems
+})
+
+export default connect(mapPropsToState)(CartDropdown);
