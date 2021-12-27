f5d443a93dc5a376af06093c963afc6436c13356
andyluak <49715292+andyluak@users.noreply.github.com>
2021-12-27 11:43:25 +0200

    Sign In Component

--- peregrine-shop/src/App.js
+++ peregrine-shop/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shoppage/shop';
+import SignInSignUp from './pages/sign-in-sign-up/signInSignUp';
 import Header from './components/Header/Header';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/signin" element={<SignInSignUp />} />
       </Routes>
     </div>
   );
---
+++ peregrine-shop/src/pages/sign-in-sign-up/signInSignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './signInSignUp.scss';
+
+import SignIn from '../../components/SignIn/SignIn';
+
+const SignInSignUp = () => {
+	return (
+		<div className="sign-in-sign-up">
+			<SignIn />
+		</div>
+	);
+};
+export default SignInSignUp;
